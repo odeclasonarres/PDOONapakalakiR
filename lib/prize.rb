3a4b287ccceec8f3dce47df9e4124db9e9a5398a
Odeclas <Odeclas@OdeclasHyV>
2016-10-19 17:22:39 +0200

    Estructura de clases

---
+++ lib/prize.rb
@@ -0,0 +1,14 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Prize
+  attr_reader :treasures, :level
+  def initialize(treasures, level)
+    @treasures=treasures
+    @level=level
+  end
+  def to_S
+    
+  end
+end
